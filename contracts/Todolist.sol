f77b62097db809b2b4001214076556816750a2b4
Kshitiz <sharma.kshitiz20@gmail.com>
2020-10-24 18:54:14 +0530

    create task

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -8,8 +8,14 @@ contract Todolist{
         string content;
         bool completed;
     }
+
     mapping(uint=> Task) public tasks;  
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
     constructor() public{
         createTask("Read the blockchain papers");
     }
@@ -17,5 +23,6 @@ contract Todolist{
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount]=Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
